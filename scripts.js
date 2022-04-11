3b2ab03c4e78e3b3a47291ffb2d4243cd2d71d26
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-04-11 10:34:32 -0300

    Novos Atributos no JS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
+let numeroDeCartas;
+let novasCartas = document.querySelector(ul);
 
-    const numero = prompt("Bem vindo(a) ao Parrot Card Game! Digite o número de cartas que você quer jogar: ");
+const cards = [];
+const imagensCards = [
+    "imagens/bobrossparrot.gif",
+    "imagens/explodyparrot.gif",
+    "imagens/fiestaparrot.gif",
+    "imagens/metalparrot.gif",
+    "imagens/revertitparrot.gif",
+    "imagens/tripletsparrot.gif",
+    "imagens/unicornparrot.gif"
+];
 
+inicioDoJogo();
+
+function inicioDoJogo(){
+    numeroDeCartas = Number(prompt("Bem vindo(a) ao Parrot Card Game! Digite o número de cartas par que você deseja jogar, este numero deverá ser entre 4 e 14: "));
+
+    while (numeroDeCartas === null || numeroDeCartas < 4 || numeroDeCartas > 14 || (numeroDeCartas % 2) !== 0) {
+        numeroDeCartas = Number(prompt("Digite um número par entre 4 e 14 para jogar!"));
+    }
+   adicionarCartas(); 
+}
+
+function adicionarCartas(){
+
+    let addCartas = 0
+    while(addCartas <= numeroDeCartas){
+        novasCartas.innerHTML = `<li class="card"><img src="images/front.png" alt="papagaio"></li>`;
+        addCartas++;
+    }
+}
+
+cards.sort(comparador); // Após esta linha, a Array estará embaralhada
+    
+function comparador() { 
+    return Math.random() - 0.5; 
+}
