b79d67e3cd6c415b2f90ff7d5b99c2ad3c2834d5
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-04-11 12:31:21 -0300

    Últimas modificações

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-let numeroDeCartas;
-let novasCartas = document.querySelector(ul);
+let numberOfCards;
+let newCards = document.querySelector(ul);
 
 const cards = [];
-const imagensCards = [
+const imagesCards = [
     "imagens/bobrossparrot.gif",
     "imagens/explodyparrot.gif",
     "imagens/fiestaparrot.gif",
@@ -15,25 +15,28 @@ const imagensCards = [
 inicioDoJogo();
 
 function inicioDoJogo(){
-    numeroDeCartas = Number(prompt("Bem vindo(a) ao Parrot Card Game! Digite o número de cartas par que você deseja jogar, este numero deverá ser entre 4 e 14: "));
+    numberOfCards = Number(prompt("Bem vindo(a) ao Parrot Card Game! Digite o número de cartas par que você deseja jogar, este numero deverá ser entre 4 e 14: "));
 
-    while (numeroDeCartas === null || numeroDeCartas < 4 || numeroDeCartas > 14 || (numeroDeCartas % 2) !== 0) {
-        numeroDeCartas = Number(prompt("Digite um número par entre 4 e 14 para jogar!"));
+    while (numberOfCards === null || numberOfCards < 4 || numberOfCards > 14 || (numberOfCards % 2) !== 0) {
+        numberOfCards = Number(prompt("Digite um número par entre 4 e 14 para jogar!"));
     }
    adicionarCartas(); 
 }
 
 function adicionarCartas(){
 
-    let addCartas = 0
-    while(addCartas <= numeroDeCartas){
-        novasCartas.innerHTML = `<li class="card"><img src="images/front.png" alt="papagaio"></li>`;
-        addCartas++;
+    let addCards = 0
+    while(addCards <= numberOfCards){
+        newCards.innerHTML += `
+        <li class="card"><img class="frente" src="images/front.png" alt="papagaio"></li>
+        <li class="card"><img class="verso" src="${imagesCards[i]}" alt="imagem-selecionada"></li>
+        `;
+        addCards++;
     }
 }
-
-cards.sort(comparador); // Após esta linha, a Array estará embaralhada
-    
-function comparador() { 
-    return Math.random() - 0.5; 
+function mixCards(){
+    imagesCards.sort(comparador); // Após esta linha, a Array estará embaralhada      
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }
 }
